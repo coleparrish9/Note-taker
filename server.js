3205063f7923541b20a559fd38510d11c99725eb
coleparrish9 <128101109+coleparrish9@users.noreply.github.com>
2023-10-29 09:40:53 -0700

    Finalized changes

--- server.js
+++ server.js
@@ -1,31 +1,65 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const apiRouter = require('./public/routes/apiRoutes');
-const htmlRouter = require('./public/routes/htmlRoutes');
+const express = require("express");
+const fs = require("fs");
 const app = express();
+const uuid = require("uuid");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRouter);
-app.use('/html', htmlRouter);
 
-app.get('/', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'routes', 'index.html'));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/routes/index.html"));
 });
 
-app.get('/html', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'routes', 'index.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/routes/notes.html"));
 });
 
-app.get('/notes', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'routes', 'notes.html'));
+app.get("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes);
 });
 
+app.post("/api/notes", (req, res) => {
+  const currentNotes = fs.readFileSync("./db/db.json", "utf8");
+  let parsedNotes = JSON.parse(currentNotes);
+  const { title, text } = req.body;
+
+    const newNote = {
+      title,
+      text,
+      id: uuid.v4(),
+    };
+
+    //
+    parsedNotes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(parsedNotes));
+    res.json(parsedNotes);
+  }
+);    
+
+
+
+app.delete("/api/notes/:id", (req, res) => {
+  const currentNotes = fs.readFileSync("./db/db.json", "utf-8");
+  let parsedNotes = JSON.parse(currentNotes);
+  const noteID = req.params.id;
+
+  const deleteNote = parsedNotes.find((note) => note.id === noteID);
+  if (deleteNote) {
+    parsedNotes = parsedNotes.filter(note => note.id !== noteID);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(parsedNotes));
+  res.json(parsedNotes);
+  }
+});  
+    
 app.listen(PORT, () => {
-    console.log(`Server is listening on http://127.0.0.1:${PORT}`);
+  console.log(`Server is available on http://localhost:${PORT}`);
 });
