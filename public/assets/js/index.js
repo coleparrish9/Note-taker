5ccd9e3ca645a65ec56412b55e86577b7ac87724
coleparrish9 <128101109+coleparrish9@users.noreply.github.com>
2023-10-29 10:10:28 -0700

    Re wrote a couple of things

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,51 +1,3 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
-
-if (window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title');
-  noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
-}
-
-const show = (elem) => {
-  elem.style.display = 'inline';
-};
-
-const hide = (elem) => {
-  elem.style.display = 'none';
-};
-
-let activeNote = {};
-
-const getNotes = () =>
-  fetch('/api/notes', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-const saveNote = (note) =>
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(note),
-  });
-
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
@@ -66,7 +18,7 @@ const renderActiveNote = () => {
 const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
-    text: noteText.value,
+    text: noteText.value
   };
   saveNote(newNote).then(() => {
     getAndRenderNotes();
--- server.js
+++ server.js
@@ -13,53 +13,13 @@ app.use(express.json());
 
 
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/routes/index.html"));
-});
+const path = require('path');
+const app = require('./routes/index.js');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const router = express.Router();
+const { readFromFile, writeToFile, readAndAppend } = require('./helpers');
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/routes/notes.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  res.json(notes);
-});
-
-app.post("/api/notes", (req, res) => {
-  const currentNotes = fs.readFileSync("./db/db.json", "utf8");
-  let parsedNotes = JSON.parse(currentNotes);
-  const { title, text } = req.body;
-
-    const newNote = {
-      title,
-      text,
-      id: uuid.v4(),
-    };
-
-    //
-    parsedNotes.push(newNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(parsedNotes));
-    res.json(parsedNotes);
-  }
-);    
-
-
-
-app.delete("/api/notes/:id", (req, res) => {
-  const currentNotes = fs.readFileSync("./db/db.json", "utf-8");
-  let parsedNotes = JSON.parse(currentNotes);
-  const noteID = req.params.id;
-
-  const deleteNote = parsedNotes.find((note) => note.id === noteID);
-  if (deleteNote) {
-    parsedNotes = parsedNotes.filter(note => note.id !== noteID);
-
-  fs.writeFileSync("./db/db.json", JSON.stringify(parsedNotes));
-  res.json(parsedNotes);
-  }
-});  
-    
 app.listen(PORT, () => {
-  console.log(`Server is available on http://localhost:${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
