54f435364cea149c0f99b302d856c1fa65515d90
coleparrish9 <128101109+coleparrish9@users.noreply.github.com>
2023-10-30 09:58:20 -0700

    Update

--- server.js
+++ server.js
@@ -1,59 +1,25 @@
-const express = require("express");
-const fs = require("fs");
-const app = express();
-const uuid = require("uuid");
-const fs = require('fs');
-const uuid = require('uuid');
-const path = require('path'); 
-const express = require('express'); 
-const App = express();
-const JSONnotes = require('./db/db.json');
+const express = require('express');
+
+const path = require('path');
+const api = require('./routes/index')
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
-app.use(express.json());
+const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/routes/index.html'))
-});
+app.use(express.json());
+app.use(express.static('public'))
+
+app.use('/api', api)
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/routes/notes.html'))
-});
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
 
-app.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    const {title, text} = req.body;
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuid.v4(),
-        };
-        JSONnotes.push(newNote);
-
-        fs.writeFileSync('./db/db.json', JSON.stringify(JSONnotes, null, 4));
-        res.json(JSONnotes);
-    }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const noteID = req.params.id;
-    const deleteThisNote = JSONnotes.find((note) => note.id === noteID);
-    JSONnotes.splice(deleteThisNote, 1);
-    fs.writeFileSync('./db/db.json', JSON.stringify(JSONnotes, null, 4));
-    res.json(JSONnotes);
-});
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/routes/index.html'))
-);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+    console.log(`Listening at http://localhost:${PORT}}`)
+})
