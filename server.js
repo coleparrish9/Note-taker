40abd3671d885e6356ad6aa03a8d8a31e819d0a8
coleparrish9 <128101109+coleparrish9@users.noreply.github.com>
2023-10-30 19:34:11 -0700

    hopefully

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './pub/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './pub/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => { 
