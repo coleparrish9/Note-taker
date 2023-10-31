505b31bf6afc58563821c3471d93059af3e7819a
coleparrish9 <128101109+coleparrish9@users.noreply.github.com>
2023-10-31 07:06:45 -0700

    Changed a couple of things trying for fixed code

---
+++ pub/routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const uuid = require('uuid');
+const fs = require('fs');
+
+router.get('/notes', async (req, res) => {
+    try {
+        const data = await fs.promises.readFile('./db/db.json', 'utf8');
+        res.json(JSON.parse(data));
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Failed to read DB.'});
+    }
+});
+
+router.post('/notes', async (req, res) => {
+    try {
+        const data = await fs.promises.readFile('./db/db.json', 'utf8');
+        const notes = JSON.parse(data);
+        const newNote = {
+            id: uuid.v4(),
+            title: req.body.title,
+            text: req.body.text
+        };
+        notes.push(newNote);
+        await fs.promises.writeFile('./db/db.json', JSON.stringify(notes));
+        res.json(newNote);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Failed to save note.'});
+    }
+});
+
+router.delete('/notes/:id', async (req, res) => {
+    try {
+        const data = await fs.promises.readFile('./db/db.json', 'utf8');
+        const notes = JSON.parse(data);
+        const filteredNotes = notes.filter(note => note.id !== req.params.id);
+        await fs.promises.writeFile('./db/db.json', JSON.stringify(filteredNotes));
+        res.json({ok: true});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Failed to delete note.'});
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,61 +1,20 @@
-const fs = require('fs');
-const uuid = require('uuid');
-const path = require('path');
+
 const express = require('express');
-
-const app = express();
-
-const DBnotes = require('./db/db.json');
+const htmlRoutes = require('./public/routes/htmlRoutes');
+const apiRoutes = require('./public/routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
 
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
-app.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    const {title, text} = req.body;
-
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuid.v4(),
-        };
-
-        DBnotes.push(newNote);
-        fs.writeFileSync('./db/db.json', JSON.stringify(DBnotes, null, 4));
-        res.json(DBnotes);
-    }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const noteID = req.params.id;
-    const deleteThisNote = DBnotes.find((note) => note.id === noteID);
-
-    DBnotes.splice(deleteThisNote, 1);
-    fs.writeFileSync('./db/db.json', JSON.stringify(DBnotes, null, 4));
-    res.json(DBnotes);
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-
-app.listen(PORT, () => {
-    console.log(`listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    );
